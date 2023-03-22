609d192edecbf67f8cd20ee2bc061ba513c391ef
mariano <marianomosca96@gmail.com>
2023-03-22 14:02:33 +0100

    ideas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hola 3er proyecto!</div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>DISCO DURO ONLINE</h1>
+      </header>
+      <nav>
+        Nav para buscar archivos o carpetas por ejemplo, aunque viendo el modelo
+        de Google Drive aqui tiene un form donde directamente buscas el archivo
+        o carpeta!
+      </nav>
+      <section>
+        <p>
+          Podemos crear una seccion para los archivos y carpetas, o separar dos
+          secciones una para cada.
+        </p>
+      </section>
+      <footer>Hack a Boss@</footer>
+    </div>
+  );
 }
 
 export default App;
