b8d0d10826cf37e60ed37b2198bc637550a7ba7e
mariano <marianomosca96@gmail.com>
2023-04-26 18:40:06 +0200

    fix

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,22 +1,11 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { Link } from "react-router-dom";
-import { Avatar } from "./Avatar";
 
 export const Auth = () => {
   const { user, logout } = useContext(AuthContext);
 
   return user ? (
-<<<<<<< HEAD
-    <>
-      <section>
-        Conectado como
-        <Link to="/user">{user.name}</Link>
-        <Avatar user={user} />
-        <button onClick={() => logout()}> Salir</button>
-      </section>
-    </>
-=======
     <section className="auth">
       Conectado como
       <Link to="/user">
@@ -24,6 +13,5 @@ export const Auth = () => {
       </Link>
       <button onClick={() => logout()}> Salir</button>
     </section>
->>>>>>> mariano
   ) : null;
 };
